46fa73e32eb7226d069acfc5e8086895694f979f
darkfire0404 <81874150+darkfire0404@users.noreply.github.com>
2021-04-03 22:35:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function show(){
+    alert("Нажми на photo и я покажу тебе фотку :)");
+    new Audio("image/mymusic.mp3").play();
+}
+//window.onload=()=>{
+//  new Audio("image/mymusic.mp3").play();
+//}
